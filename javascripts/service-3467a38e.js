f120d39774e1f456e60911290d84acab9e24cff1
Loic Stas <lolostas@hotmail.com>
2018-01-31 16:31:13 +0100

    Automated commit at 2018-01-31 15:31:11 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/service-3467a38e.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".tab").on("click",function(){$(".tab").removeClass("active"),$(this).addClass("active"),$(".tab-content").addClass("hidden"),$($(this).data("target")).removeClass("hidden")})});
